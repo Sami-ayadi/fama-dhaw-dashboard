73d1e3b369658530dbbc8da0412fa91837780794
Sami-ayadi <129682959+Sami-ayadi@users.noreply.github.com>
2026-07-24 10:56:18 +0100

    Update scraper.py

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,12 +1,15 @@
 """
-Famma Dhaw Scraper - Collecte des donnees de coupures d'electricite en Tunisie
+Famma Dhaw Scraper - Collecte des coupures d'electricite en Tunisie
 """
 import os
 import requests
 import sqlite3
 import random
 from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    BeautifulSoup = None
 import re
 import logging
 
@@ -17,22 +20,22 @@ DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "outa
 FAMMA_URL = "https://famma-dhaw.com/"
 
 ZONES_DATA = {
-    "Tunis": ['Ain Zaghouan', 'Ain Zaghouan Nord', 'Alain Savary', 'Ariana Ville', 'Attarine', 'Bab Bhar', 'Bardo', 'Belvedere', 'Ben Arous', 'Berges du Lac 1', 'Berges du Lac 2', 'Carthage', 'Centre Urbain Nord', 'Cite El Khadra', 'Den Den', 'Douar Hicher', 'El Aouina', 'El Hrairia', 'El Kabaria', 'El Kram', 'El Manar', 'El Menzah 1', 'El Menzah 4', 'El Menzah 5', 'El Menzah 6', 'El Menzah 7', 'El Menzah 8', 'El Menzah 9', 'El Omrane', 'El Ouardia', 'Ettadhamen', 'Ezzouhour', 'Gammarth', 'Gorjani', 'Hafsia', 'Hammam Lif', 'Jardins de Carthage', 'Jebel Jelloud', 'La Goulette', 'La Marsa', 'La Soukra', 'Lac 1', 'Lac 2', 'Le Bardo', 'Manouba', 'Medina', 'Megrine', 'Menzah 3', 'Mohamed Ali', 'Montplaisir', 'Mutuelleville', 'Nouvelle Medina', 'Place de Barcelone', 'Rades', 'Sidi Bou Said', 'Sidi El Bechir', 'Sidi Hassine', 'Sidi Thabet', 'Sijoumi', 'Tahar Haddad', 'Tunis Centre', 'Zahrouni'],
-    "Sfax": ['Route Menzel Chaker', 'Sfax Ville', 'Sfax Est', 'Sfax Ouest', 'Sfax Sud', 'Sfax Nord', 'Sfax Centre', 'Sakiet Ezzit', 'Sakiet Eddaier', 'Chihia', 'El Ain', 'Gremda', 'Kerkennah', 'Mahres', 'Sidi Mansour', 'Thyna', 'Agareb', 'Bir Ali Ben Khalifa', 'El Amra', 'Esskhira', 'Jebiniana', 'Kerkennah Nord', 'Kerkennah Sud', 'Menzel Chaker', 'Menzel Chaker Centre', 'Route de Gabes', 'Route de Mahdia', 'Route de Menzel', 'Route de Tunis', 'Sfax El Jadida', 'Sfax Medina', 'Sfax Port', 'Sidi Abbes', 'Skhira', 'Teboulba', 'Thyna Nord', 'Thyna Sud', 'Tina', 'Zitouna'],
+    "Tunis": ['Ain Zaghouan', 'Ariana Ville', 'Attarine', 'Bab Bhar', 'Bardo', 'Belvedere', 'Ben Arous', 'Berges du Lac 1', 'Berges du Lac 2', 'Carthage', 'Centre Urbain Nord', 'Cite El Khadra', 'Den Den', 'Douar Hicher', 'El Aouina', 'El Hrairia', 'El Kabaria', 'El Kram', 'El Manar', 'El Menzah 1', 'El Menzah 4', 'El Menzah 5', 'El Menzah 6', 'El Menzah 7', 'El Menzah 8', 'El Omrane', 'El Ouardia', 'Ettadhamen', 'Ezzouhour', 'Gammarth', 'Gorjani', 'Hafsia', 'Hammam Lif', 'Jardins de Carthage', 'Jebel Jelloud', 'La Goulette', 'La Marsa', 'La Soukra', 'Lac 1', 'Lac 2', 'Le Bardo', 'Manouba', 'Medina', 'Megrine', 'Menzah 3', 'Mohamed Ali', 'Montplaisir', 'Mutuelleville', 'Nouvelle Medina', 'Place de Barcelone', 'Rades', 'Sidi Bou Said', 'Sidi El Bechir', 'Sidi Hassine', 'Sidi Thabet', 'Sijoumi', 'Tahar Haddad', 'Tunis Centre'],
+    "Sfax": ['Route Menzel Chaker', 'Sfax Ville', 'Sfax Est', 'Sfax Ouest', 'Sfax Sud', 'Sfax Nord', 'Sfax Centre', 'Sakiet Ezzit', 'Sakiet Eddaier', 'Chihia', 'El Ain', 'Gremda', 'Kerkennah', 'Mahres', 'Sidi Mansour', 'Thyna', 'Agareb', 'Bir Ali Ben Khalifa', 'El Amra', 'Esskhira', 'Jebiniana', 'Kerkennah Nord', 'Kerkennah Sud', 'Menzel Chaker', 'Menzel Chaker Centre', 'Sfax El Jadida', 'Sfax Medina', 'Sfax Port', 'Sidi Abbes', 'Skhira', 'Teboulba', 'Tina', 'Zitouna'],
     "Sousse": ['Sousse Ville', 'Sousse Erriadh', 'Sousse Jaouhara', 'Sousse Medina', 'Akouda', 'Bouficha', 'Enfidha', 'Hammam Sousse', 'Hergla', 'Kalaa Kebira', 'Kalaa Seghira', 'Kondar', "M'saken", 'Sidi Bou Ali', 'Sidi El Hani', 'Sousse Centre', 'Sousse Est', 'Sousse Khzema', 'Sousse Nord', 'Sousse Ouest', 'Sousse Riadh', 'Sousse Sahline', 'Sousse Sidi Abdelhamid', 'Sousse Ville Nouvelle', 'Zaouiet Sousse'],
-    "Kairouan": ['Kairouan Nord', 'Kairouan Sud', 'Kairouan Ville', 'Bou Hajla', 'Chebika', 'Echrarda', 'El Alaa', 'Haffouz', 'Hajeb El Ayoun', 'Nasrallah', 'Oueslatia', 'Sbikha', 'Sidi Bou Ali', 'Sidi Saad', 'Bir Lahmer', 'Bou Hajla Centre', 'El Ain El Beidha', 'El Borj', 'El Ghaba', 'El Ghorra', 'El Haouaria', 'El Khadhra', 'El Ksour', 'El Maaguela', 'El Menzel', 'Ennasr', 'Errahma', 'Essaada', 'Essalem', 'Ezzouhour', 'Kairouan Centre', 'Kairouan Est', 'Kairouan Medina', 'Kairouan Ouest', 'Kairouan Ville Nouvelle', 'Nasrallah Centre', 'Ouled Farhan', 'Sidi Bouzid Nord', 'Sidi Bouzid Sud', 'Sidi Saad Centre'],
-    "Sidi Bouzid": ['Sidi Bouzid Ville', 'Bir El Hafey', 'Cebbala', 'Jilma', 'Meknassy', 'Menzel Bouzaiane', 'Ouled Haffouz', 'Regueb', 'Sidi Ali Ben Aoun', 'Souk Jedid', 'Bir Lahmar', 'El Ahouaz', 'El Ferch', 'El Ghaba', 'El Guettar', 'El Hencha', 'El Ksour', 'Essaida', 'Ezzouhour', 'Menzel Bouziane Centre', 'Ouled Farhan', 'Regueb Centre', 'Sidi Bouzid Centre', 'Sidi Bouzid Est', 'Sidi Bouzid Ouest'],
+    "Kairouan": ['Kairouan Nord', 'Kairouan Sud', 'Kairouan Ville', 'Bou Hajla', 'Chebika', 'Echrarda', 'El Alaa', 'Haffouz', 'Hajeb El Ayoun', 'Nasrallah', 'Oueslatia', 'Sbikha', 'Sidi Saad', 'El Ain El Beidha', 'El Borj', 'El Ghaba', 'El Ghorra', 'El Haouaria', 'El Khadhra', 'El Ksour', 'El Maaguela', 'El Menzel', 'Ennasr', 'Errahma', 'Essaada', 'Essalem', 'Ezzouhour', 'Kairouan Centre', 'Kairouan Est', 'Kairouan Medina', 'Kairouan Ouest', 'Kairouan Ville Nouvelle', 'Nasrallah Centre', 'Ouled Farhan', 'Sidi Bouzid Nord', 'Sidi Bouzid Sud', 'Sidi Saad Centre'],
+    "Sidi Bouzid": ['Sidi Bouzid Ville', 'Bir El Hafey', 'Cebbala', 'Jilma', 'Meknassy', 'Menzel Bouzaiane', 'Ouled Haffouz', 'Regueb', 'Sidi Ali Ben Aoun', 'Souk Jedid', 'Bir Lahmar', 'El Ahouaz', 'El Ferch', 'El Guettar', 'El Hencha', 'El Ksour', 'Essaida', 'Ezzouhour', 'Menzel Bouziane Centre', 'Ouled Farhan', 'Regueb Centre', 'Sidi Bouzid', 'Sidi Bouzid Centre', 'Sidi Bouzid Est', 'Sidi Bouzid Ouest'],
     "Gabes": ['Gabes Medina', 'Gabes Ville', 'Gabes Ouest', 'Gabes Sud', 'El Hamma', 'El Metouia', 'Ghannouch', 'Mareth', 'Matmata', 'Nouvelle Matmata', 'Oudhref', 'Zarat', 'Bou Attouche', 'Chenini Nahal', 'El Aouinet', 'El Grine', 'El Ksar', 'Gabes Centre', 'Gabes Est', 'Gabes Nord', 'Gabes Port', 'Gabes Ville Nouvelle', 'Mareth Centre', 'Matmata Centre', 'Menzel El Habib'],
-    "Gafsa": ['Gafsa Ville', 'Belkhir', 'El Guettar', 'El Ksar', 'El Mdhilla', 'Gafsa Nord', 'Gafsa Sud', 'Mdhilla', 'Metlaoui', 'Moulares', 'Redeyef', 'Sened', 'Sned', 'Belkhir Centre', 'El Guettar Centre', 'El Ksar Centre', 'Gafsa Centre', 'Gafsa Est', 'Gafsa Medina', 'Gafsa Ouest', 'Metlaoui Centre', 'Moulares Centre', 'Redeyef Centre', 'Sened Centre', 'Sned Centre'],
-    "Monastir": ['Monastir Ville', 'Bekalta', 'Bembla', 'Beni Hassen', 'Jemmal', 'Ksar Hellal', 'Ksibet El Mediouni', 'Moknine', 'Ouerdanine', 'Sahline', 'Sayada', 'Teboulba', 'Zeramdine', 'Bekalta Centre', 'Bembla Centre', 'Beni Hassen Centre', 'Jemmal Centre', 'Ksar Hellal Centre', 'Ksibet El Mediouni Centre', 'Moknine Centre', 'Monastir Centre', 'Monastir Est', 'Monastir Nord', 'Monastir Ouest', 'Monastir Sud', 'Ouerdanine Centre', 'Sahline Centre', 'Sayada Centre', 'Teboulba Centre', 'Zeramdine Centre'],
-    "Mahdia": ['Mahdia Ville', 'Bou Merdes', 'Chebba', 'Chorbane', 'El Jem', 'Essouassi', 'Hebira', 'Kerker', 'Ksour Essaf', 'Mellouleche', 'Ouled Chamekh', 'Sidi Alouane', 'Bou Merdes Centre', 'Chebba Centre', 'Chorbane Centre', 'El Jem Centre', 'Essouassi Centre', 'Hebira Centre', 'Kerker Centre', 'Ksour Essaf Centre', 'Mahdia Centre', 'Mahdia Est', 'Mahdia Nord', 'Mahdia Ouest', 'Mahdia Sud', 'Mellouleche Centre', 'Ouled Chamekh Centre', 'Sidi Alouane Centre'],
-    "Nabeul": ['Nabeul Ville', 'Beni Khalled', 'Beni Khiar', 'Bou Argoub', 'Dar Chaabane', 'El Haouaria', 'El Mida', 'Grombalia', 'Hammam Ghezeze', 'Hammamet', 'Kelibia', 'Korba', 'Menzel Bouzelfa', 'Menzel Temime', 'Soliman', 'Takelsa', 'Tazarka', 'Zriba', 'Beni Khalled Centre', 'Beni Khiar Centre', 'Bou Argoub Centre', 'Dar Chaabane Centre', 'El Haouaria Centre', 'El Mida Centre', 'Grombalia Centre', 'Hammam Ghezeze Centre', 'Hammamet Centre', 'Hammamet Nord', 'Hammamet Sud', 'Kelibia Centre', 'Kelibia Nord', 'Kelibia Sud', 'Korba Centre', 'Menzel Bouzelfa Centre', 'Menzel Temime Centre', 'Nabeul Centre', 'Nabeul Est', 'Nabeul Nord', 'Nabeul Ouest', 'Nabeul Sud', 'Soliman Centre', 'Takelsa Centre', 'Tazarka Centre', 'Zriba Centre'],
-    "Bizerte": ['Bizerte Ville', 'Bizerte Nord', 'Bizerte Sud', 'Djebel Ichkeul', 'El Alia', 'Ghar El Melh', 'Mateur', 'Menzel Bourguiba', 'Menzel Jemil', 'Ras Jebel', 'Sejnane', 'Tinja', 'Utique', 'Zarzouna', 'Bizerte Centre', 'Bizerte Est', 'Bizerte Medina', 'Bizerte Ouest', 'Bizerte Port', 'Bizerte Ville Nouvelle', 'Djebel Ichkeul Centre', 'El Alia Centre', 'Ghar El Melh Centre', 'Mateur Centre', 'Menzel Bourguiba Centre', 'Menzel Jemil Centre', 'Ras Jebel Centre', 'Sejnane Centre', 'Tinja Centre', 'Utique Centre', 'Zarzouna Centre'],
+    "Gafsa": ['Gafsa Ville', 'Belkhir', 'El Guettar', 'El Ksar', 'El Mdhilla', 'Gafsa Nord', 'Gafsa Sud', 'Mdhilla', 'Metlaoui', 'Moulares', 'Redeyef', 'Sened', 'Sned', 'El Guettar Centre', 'El Ksar Centre', 'Gafsa Centre', 'Gafsa Est', 'Gafsa Medina', 'Gafsa Ouest', 'Metlaoui Centre', 'Sened Centre'],
+    "Monastir": ['Monastir Ville', 'Bekalta', 'Bembla', 'Beni Hassen', 'Jemmal', 'Ksar Hellal', 'Ksibet El Mediouni', 'Moknine', 'Ouerdanine', 'Sahline', 'Sayada', 'Teboulba', 'Zeramdine', 'Bekalta Centre', 'Bembla Centre', 'Beni Hassen Centre', 'Jemmal Centre', 'Ksar Hellal Centre', 'Ksibet El Mediouni Centre', 'Moknine Centre', 'Monastir Centre', 'Monastir Est', 'Monastir Nord', 'Monastir Sud'],
+    "Mahdia": ['Mahdia Ville', 'Bou Merdes', 'Chebba', 'Chorbane', 'El Jem', 'Essouassi', 'Hebira', 'Kerker', 'Ksour Essaf', 'Mellouleche', 'Ouled Chamekh', 'Sidi Alouane', 'Bou Merdes Centre', 'Chebba Centre', 'Chorbane Centre', 'El Jem Centre', 'Essouassi Centre', 'Hebira Centre', 'Kerker Centre', 'Ksour Essaf Centre', 'Mahdia Centre', 'Mahdia Est', 'Mahdia Nord', 'Mahdia Ouest', 'Mahdia Sud'],
+    "Nabeul": ['Nabeul Ville', 'Beni Khalled', 'Beni Khiar', 'Bou Argoub', 'Dar Chaabane', 'El Haouaria', 'El Mida', 'Grombalia', 'Hammam Ghezeze', 'Hammamet', 'Kelibia', 'Korba', 'Menzel Bouzelfa', 'Menzel Temime', 'Soliman', 'Takelsa', 'Tazarka', 'Zriba', 'Beni Khalled Centre', 'Beni Khiar Centre', 'Bou Argoub Centre', 'Dar Chaabane Centre', 'El Haouaria Centre', 'El Mida Centre', 'Grombalia Centre', 'Hammam Ghezeze Centre', 'Hammamet Centre', 'Hammamet Nord', 'Hammamet Sud', 'Kelibia Centre', 'Kelibia Nord', 'Kelibia Sud', 'Korba Centre', 'Menzel Bouzelfa Centre', 'Menzel Temime Centre', 'Nabeul Centre', 'Nabeul Nord', 'Nabeul Sud'],
+    "Bizerte": ['Bizerte Ville', 'Bizerte Nord', 'Bizerte Sud', 'Djebel Ichkeul', 'El Alia', 'Ghar El Melh', 'Mateur', 'Menzel Bourguiba', 'Menzel Jemil', 'Ras Jebel', 'Sejnane', 'Tinja', 'Utique', 'Zarzouna', 'Bizerte Centre', 'Bizerte Est', 'Bizerte Medina', 'Bizerte Ouest', 'Bizerte Port', 'Djebel Ichkeul Centre', 'El Alia Centre', 'Ghar El Melh Centre', 'Mateur Centre', 'Menzel Bourguiba Centre', 'Menzel Jemil Centre', 'Ras Jebel Centre', 'Sejnane Centre', 'Tinja Centre', 'Utique Centre', 'Zarzouna Centre'],
     "Beja": ['Beja Ville', 'Amdoun', 'Nefza', 'Teboursouk', 'Testour', 'Thibar', 'Beja Centre', 'Beja Est', 'Beja Nord', 'Beja Ouest', 'Beja Sud', 'Nefza Centre', 'Teboursouk Centre', 'Testour Centre', 'Thibar Centre'],
-    "Jendouba": ['Jendouba Ville', 'Ain Draham', 'Balta', 'Bou Salem', 'Fernana', 'Ghardimaou', 'Jendouba Nord', 'Jendouba Sud', 'Oued Meliz', 'Tabarka', 'Jendouba Centre', 'Jendouba Est', 'Jendouba Ouest', 'Ain Draham Centre', 'Balta Centre', 'Bou Salem Centre', 'Fernana Centre', 'Ghardimaou Centre', 'Oued Meliz Centre', 'Tabarka Centre'],
-    "Kef": ['Kef Ville', 'Dahmani', 'Jerissa', 'Kalaa Senan', 'Kalaat Khasba', 'Kef Est', 'Kef Ouest', 'Le Kef', 'Nebeur', 'Sakiet Sidi Youssef', 'Sers', 'Tajerouine', 'Touiref', 'Dahmani Centre', 'Jerissa Centre', 'Kalaa Senan Centre', 'Kef Centre', 'Kef Nord', 'Kef Sud', 'Nebeur Centre', 'Sakiet Sidi Youssef Centre', 'Sers Centre', 'Tajerouine Centre', 'Touiref Centre'],
-    "Siliana": ['Siliana Ville', 'Bargou', 'Bou Arada', 'El Aroussa', 'El Krib', 'Gaafour', 'Kesra', 'Makthar', 'Rouhia', 'Sidi Bou Rouis', 'Siliana Centre', 'Siliana Nord', 'Siliana Sud', 'Bargou Centre', 'Bou Arada Centre', 'El Aroussa Centre', 'El Krib Centre', 'Gaafour Centre', 'Kesra Centre', 'Makthar Centre', 'Rouhia Centre', 'Sidi Bou Rouis Centre'],
-    "Kasserine": ['Kasserine Ville', 'El Ayoun', 'Ezzouhour', 'Feriana', 'Foussana', 'Haidra', 'Hassi El Ferid', 'Jedelienne', 'Kasserine Nord', 'Kasserine Sud', 'Mejel Bel Abbes', 'Sbeitla', 'Sbiba', 'Thala', 'El Ayoun Centre', 'Ezzouhour Centre', 'Feriana Centre', 'Foussana Centre', 'Haidra Centre', 'Hassi El Ferid Centre', 'Jedelienne Centre', 'Kasserine Centre', 'Kasserine Est', 'Kasserine Ouest', 'Mejel Bel Abbes Centre', 'Sbeitla Centre', 'Sbiba Centre', 'Thala Centre'],
+    "Jendouba": ['Jendouba Ville', 'Ain Draham', 'Balta', 'Bou Salem', 'Fernana', 'Ghardimaou', 'Jendouba Nord', 'Jendouba Sud', 'Oued Meliz', 'Tabarka', 'Jendouba Centre', 'Jendouba Est', 'Jendouba Ouest', 'Balta Centre', 'Bou Salem Centre', 'Fernana Centre', 'Ghardimaou Centre', 'Oued Meliz Centre', 'Tabarka Centre'],
+    "Kef": ['Kef Ville', 'Dahmani', 'Jerissa', 'Kalaa Senan', 'Kalaat Khasba', 'Kef Est', 'Kef Ouest', 'Le Kef', 'Nebeur', 'Sakiet Sidi Youssef', 'Sers', 'Tajerouine', 'Dahmani Centre', 'Jerissa Centre', 'Kalaa Senan Centre', 'Kef Centre', 'Kef Nord', 'Kef Sud', 'Nebeur Centre', 'Sakiet Sidi Youssef Centre', 'Sers Centre', 'Tajerouine Centre'],
+    "Siliana": ['Siliana Ville', 'Bargou', 'Bou Arada', 'El Aroussa', 'El Krib', 'Gaafour', 'Kesra', 'Makthar', 'Rouhia', 'Sidi Bou Rouis', 'Siliana Centre', 'Siliana Nord', 'Siliana Sud', 'Bargou Centre', 'Bou Arada Centre', 'El Aroussa Centre', 'El Krib Centre', 'Gaafour Centre', 'Kesra Centre', 'Makthar Centre', 'Rouhia Centre'],
+    "Kasserine": ['Kasserine Ville', 'El Ayoun', 'Ezzouhour', 'Feriana', 'Foussana', 'Haidra', 'Hassi El Ferid', 'Jedelienne', 'Kasserine Nord', 'Kasserine Sud', 'Mejel Bel Abbes', 'Sbeitla', 'Sbiba', 'Thala', 'El Ayoun Centre', 'Ezzouhour Centre', 'Feriana Centre', 'Foussana Centre', 'Haidra Centre', 'Hassi El Ferid Centre', 'Jedelienne Centre', 'Kasserine Centre', 'Kasserine Est', 'Kasserine Ouest'],
     "Tataouine": ['Tataouine Ville', 'Bir Lahmar', 'Dhehiba', 'Ghomrassen', 'Remada', 'Smar', 'Tataouine Nord', 'Tataouine Sud', 'Bir Lahmar Centre', 'Dhehiba Centre', 'Ghomrassen Centre', 'Remada Centre', 'Smar Centre', 'Tataouine Centre', 'Tataouine Est', 'Tataouine Ouest'],
     "Medenine": ['Medenine Ville', 'Ben Gardane', 'Beni Khedache', 'Djerba', 'Djerba Ajim', 'Djerba Houmt Souk', 'Djerba Midoun', 'Sidi Makhlouf', 'Zarzis', 'Ajim', 'Ben Gardane Centre', 'Beni Khedache Centre', 'Djerba Centre', 'Houmt Souk', 'Medenine Centre', 'Medenine Est', 'Medenine Nord', 'Medenine Ouest', 'Medenine Sud', 'Midoun', 'Sidi Makhlouf Centre', 'Zarzis Centre', 'Zarzis Nord', 'Zarzis Sud'],
     "Tozeur": ['Tozeur Ville', 'Degache', 'El Hamma', 'Nefta', 'Tameghza', 'Tozeur Centre', 'Tozeur Est', 'Tozeur Nord', 'Tozeur Ouest', 'Tozeur Sud', 'Degache Centre', 'El Hamma Centre', 'Nefta Centre', 'Tameghza Centre'],
@@ -57,7 +60,7 @@ class FammaScraper:
         self.init_db()
         self.last_scrape = None
         self.last_scrape_success = False
-
+        
     def init_db(self):
         data_dir = os.path.dirname(DB_PATH)
         if not os.path.exists(data_dir):
@@ -104,8 +107,11 @@ class FammaScraper:
         conn.commit()
         conn.close()
         logger.info("Database initialized")
-
+    
     def scrape_famma(self):
+        if BeautifulSoup is None:
+            logger.warning("BeautifulSoup not available, using simulation")
+            return None
         try:
             logger.info(f"Scraping {FAMMA_URL}...")
             response = self.session.get(FAMMA_URL, timeout=15)
@@ -134,7 +140,7 @@ class FammaScraper:
             logger.error(f"Scraping failed: {e}")
             self.last_scrape_success = False
             return None
-
+    
     def _parse_cards(self, cards):
         parsed = []
         for card in cards:
@@ -157,7 +163,7 @@ class FammaScraper:
                 light_reports = int(numbers[1])
             parsed.append({"name": zone_name, "status": status, "reports_dark": dark_reports, "reports_light": light_reports})
         return parsed
-
+    
     def generate_realistic_data(self):
         now = datetime.now()
         hour = now.hour
@@ -168,8 +174,8 @@ class FammaScraper:
         else:
             base_prob = 0.12
         gov_multipliers = {"Sfax": 1.45, "Sidi Bouzid": 1.35, "Kairouan": 1.30, "Gafsa": 1.25, "Gabes": 1.20, "Kasserine": 1.18, "Tataouine": 1.15, "Medenine": 1.12, "Kebili": 1.10, "Tozeur": 1.08, "Siliana": 1.05, "Beja": 1.02, "Jendouba": 1.02, "Kef": 1.00, "Bizerte": 0.95, "Nabeul": 0.90, "Mahdia": 0.88, "Monastir": 0.85, "Sousse": 0.82, "Tunis": 0.75}
-        updated_zones = []
         seen = set()
+        updated_zones = []
         for zone in ALL_ZONES:
             gov = zone["governorate"]
             name = zone["name"]
@@ -195,7 +201,7 @@ class FammaScraper:
         self.last_scrape_success = True
         self.last_scrape = now
         return updated_zones
-
+    
     def update_database(self, zones_data):
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
@@ -208,7 +214,7 @@ class FammaScraper:
         conn.commit()
         conn.close()
         logger.info(f"Updated {len(zones_data)} zones in database")
-
+    
     def run_scrape(self):
         scraped = self.scrape_famma()
         if scraped and len(scraped) > 10:
@@ -219,7 +225,7 @@ class FammaScraper:
             self.update_database(simulated)
         self._clean_old_history()
         self._check_alerts()
-
+    
     def _clean_old_history(self):
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
@@ -227,7 +233,7 @@ class FammaScraper:
         c.execute("DELETE FROM history WHERE timestamp < ?", (cutoff,))
         conn.commit()
         conn.close()
-
+    
     def _check_alerts(self):
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
@@ -250,6 +256,7 @@ class FammaScraper:
                          ("PRIORITY", f"Zone prioritaire coupee: {zone_name}", "HIGH"))
         conn.commit()
         conn.close()
+
 
 if __name__ == "__main__":
     scraper = FammaScraper()
